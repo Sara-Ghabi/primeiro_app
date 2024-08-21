49283ff5209521589a56852b1ff5bda86afcaa35
Sara <saraghabrielly@gmail.com>
2024-08-21 11:50:03 -0300

    Removendo imports desnecessário.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:primeiro_app/screens/register_screen.dart';
 import 'package:primeiro_app/screens/reset_password_modal.dart';
@@ -105,11 +104,12 @@ class LoginScreen extends StatelessWidget {
 
 Future<UserCredential> singinWithGoogle() async {
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-  final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+  final GoogleSignInAuthentication googleAuth =
+      await googleUser!.authentication;
   final credential = GoogleAuthProvider.credential(
     accessToken: googleAuth.accessToken,
     idToken: googleAuth.idToken,
   );
-  
+
   return await FirebaseAuth.instance.signInWithCredential(credential);
 }
